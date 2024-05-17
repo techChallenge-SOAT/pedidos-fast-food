0a877d4726158bbc79ba715c31d46316fc5e9069
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2024-05-17 18:45:11 -0300

    atualizacao-service-pedido

--- src/adapters/services/pagamento-fast-food/PagamentoFastFoodService.ts
+++ src/adapters/services/pagamento-fast-food/PagamentoFastFoodService.ts
@@ -1,14 +1,22 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export class PagamentoFastFoodService {
-  static async efetuarPagamento(id_pedido: string, valor_total: number) {
-    try {
-      await axios.post('http://servico-pagamentos/pagamentos', {
-        id_pedido,
-        valor: valor_total,
-      });
-    } catch (error) {
-      console.error('Erro ao enviar solicitação de pagamento:', error);
-    }
-  }
+    static async efetuarPagamento(id_pedido: string, valor_total: number) {
+        const url = process.env.PAGAMENTO_SERVICO_URL;
+        if (!url) {
+          throw new Error('PAGAMENTO_SERVICO_URL não está definido');
+        }
+    
+        try {
+          await axios.post(url, {
+            id_pedido,
+            valor: valor_total,
+          });
+        } catch (error) {
+          console.error('Erro ao enviar solicitação de pagamento:', error);
+        }
+      }
 }
