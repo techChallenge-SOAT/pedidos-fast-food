2a4311ed858ff791d96b4f92e50890a87d2e2b01
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2024-05-17 15:20:15 -0300

    atualizacao Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN npm run build
 
 EXPOSE 3000
 
+ENV DATABASE_URL=postgres://postgres:postgres@postgres-service:5432/bd-pedidos
+
 CMD ["npm", "start"]
--- src/adapters/http/server.ts
+++ src/adapters/http/server.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import itemRoutes from './routes/itemRoutes';
 import pedidoRoutes from './routes/pedidoRoutes';
-import { handleMercadoPagoWebhook } from './webhooks/handleMercadoPagoWebhook';
 
 const app = express();
 
@@ -11,8 +10,6 @@ app.use(bodyParser.json());
 app.use('/itens', itemRoutes);
 
 app.use('/pedidos', pedidoRoutes);
-
-app.use('/webhooks/mercadopago', handleMercadoPagoWebhook);
 
 app.get('/', (req, res) => {
   res.send('Sistema de Pedidos');
